f5664003c7a57178d3b64a93b5a6a78e92e50e88
BeGuillaume <guillaume.bernos@gmail.com>
2017-02-25 20:09:06 +0100

    test

--- DSOWeb/dso.py
+++ DSOWeb/dso.py
@@ -5,7 +5,15 @@ from pathlib import Path
 def save_file_calib(f, name, user, phone, calib):
 
     if calib:
-        os.system("mkdir " + 'save/' + user)
+        if not os.path.isdir('save/' + user):
+            os.system("mkdir " + 'save/' + user)
+            files = ["calib", "camera_calibration.cpp", "default.xml", "run.sh"]
+            for x in files:
+                os.system("cp " + 'save/' + x +' save/' + user + "/" + x)
+
+            print("Dossier User créé")
+
+
         print("Utilisateur : " + name)
         with open('save/' + user + '/calib_' + name, 'wb+') as destination:
             for chunk in f.chunks():
@@ -15,27 +23,25 @@ def save_file_calib(f, name, user, phone, calib):
 
         pwd = os.getcwd()
 
-        os.chdir(pwd + "/save")
+        os.chdir(pwd + "/save/" + user)
 
         os.system("PWD")
 
         print("Start search : " + name)
 
-        os.system("mkdir " + user + '/calib_dir' + name)
+        os.system("mkdir " + 'calib_dir' + name)
 
-        os.system("./run.sh video " + user + '/calib_' + name + " " + user + ' calib_dir' + name + " resize 640x480 search")
+        os.system("./run.sh video " + 'calib_' + name + ' calib_dir' + name + " resize 640x480 search")
 
         print("Finish search : " + name)
 
-        calib_file = open("save/camera.txt", "r")
+        os.chdir(pwd)
 
         calib_phone = Path("phone_calib/" + phone)
         if calib_phone.exists():
             return "calibexist"
         else:
-            with open('phone_calib/' + phone, 'wb+') as destination:
-                for chunk in calib_file.chunks():
-                    destination.write(chunk)
+            os.system("cp save/" + user +"/camera.txt phone_calib/" + phone)
 
             return "calibcreated"
 
